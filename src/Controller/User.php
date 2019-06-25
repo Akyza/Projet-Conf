d8c9c988d0b387b533550cb31328719ea4b8ff17
Bryce <bryce.tavares@gmail.com>
2019-06-25 16:28:11 +0200

    debut code + docker

---
+++ src/Controller/User.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class User
+ * @package App\Controller
+ * @Route(path="/users")
+ */
+class User extends AbstractController
+{
+
+}
